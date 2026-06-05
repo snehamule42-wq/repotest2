41e3df95a0592002444a75fb90ddee4190089384
Sneha Mule <snehamule42@gmail.com>
2026-06-05 12:15:04 -0400

    extract data 1

--- .github/workflows/extract_data.py
+++ .github/workflows/extract_data.py
@@ -6,7 +6,7 @@ data = {
     "id" :[1,2,3,4,5],
     "name" : ["Alice", "Bob", "Charlie", "David", "Eve"],
     "age" :[25, 30, 35, 40, 45],
-    "city" : ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"]
+    
 }
 
 df = pd.DataFrame(data)
