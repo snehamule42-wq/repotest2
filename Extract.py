7658beed23401e4326f2d28d20c3b395dbc6aeab
Sneha Mule <snehamule42@gmail.com>
2026-06-05 12:19:27 -0400

    final1

--- Extract.py
+++ Extract.py
@@ -3,11 +3,10 @@ import pandas as pd
 
 data = {
 
-    id ={1,2,3,4,5},
-    name = {"Alice", "Bob", "Charlie", "David", "Eve"},
-    age = {25, 30, 35, 40, 45},
-    city = {"New York", "Los Angeles", "Chicago", "Houston", "Phoenix
+    "id" :[1,2,3,4,5],
+    "name" : ["Alice", "Bob", "Charlie", "David", "Eve"],
+    "age" :[25, 30, 35, 40, 45]
 }
-}
+
 df = pd.DataFrame(data)
 print(df)
