65a246e5cdcc168de22ddc2a8d7a0f1cc576b1ed
Sneha Mule <snehamule42@gmail.com>
2026-06-05 12:00:56 -0400

    extract data

---
+++ .github/workflows/Extract.py
@@ -0,0 +1,13 @@
+
+import pandas as pd
+
+data = {
+
+    id ={1,2,3,4,5},
+    name = {"Alice", "Bob", "Charlie", "David", "Eve"},
+    age = {25, 30, 35, 40, 45},
+    city = {"New York", "Los Angeles", "Chicago", "Houston", "Phoenix
+}
+}
+df = pd.DataFrame(data)
+print(df)
